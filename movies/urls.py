6f6d4ff95d1404903b044e2b6428cc7042457f20
Archibald-prog <archpod@gmail.com>
2024-03-10 07:15:11 +0300

    Added url-pattern for AddReview.

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.MoviesView.as_view()),  # http://127.0.0.1:8000/
-    path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail")  # http://127.0.0.1:8000/some-slug/
+    path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
+    path('review/<int:pk>/', views.AddReview.as_view(), name="add_review"),  # http://127.0.0.1:8000/review/2/
 ]
