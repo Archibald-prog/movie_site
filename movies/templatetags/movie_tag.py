a83c3a31c5a71c44dc16d3538516ebfbbd0f3afa
Archibald-prog <archpod@gmail.com>
2024-03-15 16:56:32 +0300

    Added get_last_movies() inclusion_tag.

--- movies/templatetags/movie_tag.py
+++ movies/templatetags/movie_tag.py
@@ -1,5 +1,5 @@
 from django import template
-from movies.models import Category
+from movies.models import Category, Movie
 
 register = template.Library()
 
@@ -7,3 +7,9 @@ register = template.Library()
 @register.simple_tag()
 def get_categories():
     return Category.objects.all()
+
+
+@register.inclusion_tag('movies/tags/last_movie.html')
+def get_last_movies(count=5):
+    movies = Movie.objects.order_by("id")[:count]
+    return {"last_movies": movies}
