9337e032d379cc76d3d165a5e398f47c9ccbe460
Archibald-prog <archpod@gmail.com>
2024-03-19 10:19:23 +0300

    Added url-pattern for ActorView.

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.MoviesView.as_view()),  # http://127.0.0.1:8000/
     path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
     path('review/<int:pk>/', views.AddReview.as_view(), name="add_review"),  # http://127.0.0.1:8000/review/2/
+    path("actor/<slug:slug>/", views.ActorView.as_view(), name="actor_detail"),
 ]
