9c639bbb0b043bb930a6b5448e036906f8e912ef
Archibald-prog <archpod@gmail.com>
2024-03-25 04:47:39 +0300

    Added url-pattern for FilterMoviesView.

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.MoviesView.as_view()),  # http://127.0.0.1:8000/
+    path("filter/", views.FilterMoviesView.as_view(), name='filter'),
     path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
     path('review/<int:pk>/', views.AddReview.as_view(), name="add_review"),  # http://127.0.0.1:8000/review/2/
     path("actor/<slug:slug>/", views.ActorView.as_view(), name="actor_detail"),
